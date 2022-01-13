92d8593f562b5b9fdd07b68065431ec785d083cc
knikolov <kami_985@abv.bg>
2022-01-13 19:44:45 +0200

    Try to put the button inside the content

--- src/Document.js
+++ src/Document.js
@@ -1,20 +1,24 @@
 import { useState } from "react";
 
 export default function Document(props){
-    const [buttonRead,setButtonRead]=useState(false);
+    const [buttonDisabled,setButtonRead]=useState(true);
     return(<div>
         <h1 className = "title" >{props.title}</h1>
-            <div className = "content" style = {{overflowY:"scroll",overflowX:"auto",height : "300px"}}
+            <div className = "content"  >
+                 <div style = {{overflowY:"scroll",width:"300px",height : "300px"}}
              onScroll={(e)=> {
                 console.log(e.target.scrollHeight + " "+ e.target.scrollTop + " "+ Math.floor(e.target.scrollTop)  + " "+  e.target.clientHeight);
                 console.log(e.target.scrollHeight - Math.floor(e.target.scrollTop));
                  if((e.target.scrollHeight - Math.floor(e.target.scrollTop) === e.target.clientHeight) || (e.target.scrollHeight - Math.ceil(e.target.scrollTop) === e.target.clientHeight)){  
-                 setButtonRead((buttonRead)=>{return true;});
+                 setButtonRead((buttonDisabled)=>{return false;});
+
              }
-             }} >
-                {props.content}
+             }}
+                 
+                 >{props.content}</div>
+                   
+            <button disabled = {buttonDisabled}>I Agree</button>
             </div>
-            <button disabled = {buttonRead?"":"disabled"}>I Agree</button>
         </div>
 
     );
